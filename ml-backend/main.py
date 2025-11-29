c84422e858231d9a42c3d305c67828d9099c0063
Aishwarya Srinath <103362719+Aishusrinath@users.noreply.github.com>
2025-11-28 22:59:36 -0500

    Update main.py

--- ml-backend/main.py
+++ ml-backend/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Load model + vectorizer
 model = joblib.load("fake_job_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
+# ----------------------------
+# Enable CORS
+# ----------------------------
+origins = [
+    "https://fake-job-detect-front.onrender.com",  # Your deployed frontend
+    "http://localhost:3000"                        # Local testing
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # allow these origins
+    allow_credentials=True,
+    allow_methods=["*"],          # allow GET, POST, etc.
+    allow_headers=["*"],          # allow headers like Content-Type
+)
+
 
 class JobRequest(BaseModel):
     text: str
@@ -22,3 +38,4 @@ def predict(data: JobRequest):
     result = "FAKE" if pred == 1 else "REAL"
 
     return {"prediction": result}
+
